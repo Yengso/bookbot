c08d614b4e0c64317792d33a7dd9baf2354c022a
Yengso <sondre.engstrom@hotmail.com>
2025-08-13 23:01:15 +0200

    remove directory hardcoding and finishing touches

--- main.py
+++ main.py
@@ -1,23 +1,22 @@
 from stats import get_word_count, get_letter_count, sort_dict, sort_on
+import sys
 
 def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
         return(file_contents)
-    
-
 
 def main():
 
-    text = str(get_book_text("./books/frankenstein.txt"))
+    text = str(get_book_text(sys.argv[1]))
     the_dict = get_letter_count(text)
     dict_list = sort_dict(the_dict)
     dict_list.sort(reverse=True, key=sort_on)
 
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {sys.argv[1]}...")
     print("----------- Word Count ----------")
-    print(get_word_count("./books/frankenstein.txt"))
+    print(get_word_count(sys.argv[1]))
     print("--------- Character Count -------")
     for char in dict_list:
         print(f"{char['char']}: {char['count']}")
